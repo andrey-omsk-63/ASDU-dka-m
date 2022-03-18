9ddf043c6502d475af0121edb07be3e4e4f60e68
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-03-18 06:43:39 +0600

    '1803mor'

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => {
   const styleAppMulka = {
     border: 1,
     borderRadius: 2,
-    borderColor: 'primary.main',
+    borderColor: '#F1F5FB',
     marginTop: 0.5,
     marginRight: 5,
     marginLeft: 3,
@@ -28,9 +28,9 @@ const App = () => {
   const styleAppMenu = {
     border: 1,
     borderRadius: 1,
-    borderColor: 'primary.main',
+    borderColor: '#F1F5FB',
     marginLeft: 0.5,
-    height: '87vh',
+    height: '86vh',
     backgroundColor: '#F1F5FB',
     opacity: 0.85,
   };
@@ -39,7 +39,8 @@ const App = () => {
     border: 0,
     marginRight: 5,
     marginLeft: 3,
-    height: '3vh',
+    height: '6vh',
+    p: 1,
   };
 
   const styleHeader = {
@@ -68,123 +69,6 @@ const App = () => {
     color: 'black',
     textTransform: 'unset !important',
   };
-
-  // const styleApp01 = {
-  //   fontSize: 14,
-  //   marginRight: 0.5,
-  //   width: '18%',
-  //   maxHeight: '21px',
-  //   minHeight: '21px',
-  //   backgroundColor: '#F1F3F4',
-  //   color: 'black',
-  //   textTransform: 'unset !important',
-  // };
-
-  // const styleApp02 = {
-  //   fontSize: 14,
-  //   marginRight: 0.5,
-  //   borderRadius: 1,
-  //   width: '12%',
-  //   maxHeight: '21px',
-  //   minHeight: '21px',
-  //   backgroundColor: '#FFE295',
-  //   color: 'black',
-  //   textAlign: 'center',
-  // };
-
-  // const styleModalMenu = {
-  //   fontSize: 13.9,
-  //   maxHeight: '20px',
-  //   minHeight: '20px',
-  //   backgroundColor: '#F1F3F4',
-  //   color: 'black',
-  //   marginRight: 1,
-  //   textTransform: 'unset !important',
-  // };
-
-  // const styleModal = {
-  //   position: 'relative',
-  //   bottom: '-45vh',
-  //   marginLeft: '60vh',
-  //   transform: 'translate(-50%, -50%)',
-  //   width: 150,
-  //   bgcolor: 'background.paper',
-  //   border: '2px solid #000',
-  //   borderRadius: 2,
-  //   boxShadow: 24,
-  //   p: 3,
-  // };
-
-  // const [open, setOpen] = React.useState(false);
-  // const [crossData, setCrossData] = React.useState(0);
-
-  // const handleOpen = () => setOpen(true);
-
-  // const handleClose = (numer: number) => {
-  //   if (numer !== 777) {
-  //     setCrossData(numer);
-  //     setValue('1');
-  //     extData =
-  //       points[numer].slice(11, 13) +
-  //       '.' +
-  //       points[numer].slice(8, 10) +
-  //       '.' +
-  //       points[numer].slice(3, 7);
-  //   }
-  //   setOpen(false);
-  // };
-
-  // const SpisData = () => {
-  //   let resStr = [];
-  //   let stroka = '';
-  //   let strDat = '';
-
-  //   for (let i = 0; i < points.length; i++) {
-  //     stroka = points[i];
-  //     strDat = stroka.slice(11, 13) + '.' + stroka.slice(8, 10) + '.' + stroka.slice(3, 7);
-  //     resStr.push(
-  //       <Button key={i} sx={styleModalMenu} variant="contained" onClick={() => handleClose(i)}>
-  //         <b>{strDat}</b>
-  //       </Button>,
-  //     );
-  //   }
-  //   resStr.push(
-  //     <Button key={777} sx={styleModalMenu} variant="contained" onClick={() => handleClose(777)}>
-  //       <b>Выход</b>
-  //     </Button>,
-  //   );
-  //   return resStr;
-  // };
-
-  // const ChoiceData = () => {
-  //   return (
-  //     <>
-  //       <Button sx={styleApp01} variant="contained" onClick={handleOpen}>
-  //         <b>Выбор по дате</b>
-  //       </Button>
-  //       <Modal
-  //         open={open}
-  //         aria-labelledby="modal-modal-title"
-  //         aria-describedby="modal-modal-description">
-  //         <Box sx={styleModal}>
-  //           <Stack direction="column">{SpisData()}</Stack>
-  //         </Box>
-  //       </Modal>
-  //     </>
-  //   );
-  // };
-
-  // const [points, setPoints] = React.useState<Array<string>>([]);
-  // //const ipAdress: string = 'http://localhost:3000/otladkaGlob.json';
-  // const ipAdress = window.location.href;
-
-  // React.useEffect(() => {
-  //   axios.post(ipAdress).then(({ data }) => {
-  //     setPoints(data.fileNames);
-  //   });
-  // }, [ipAdress]);
-
-  // const [value, setValue] = React.useState('0');
 
   const [value, setValue] = React.useState('0');
 
@@ -274,11 +158,12 @@ const App = () => {
               </Stack>
             </Grid>
           </Grid>
+
           <Grid item xs sx={styleAppMenu}></Grid>
         </Grid>
         <Grid item xs={12} sx={styleAppPodv}>
           <Box sx={{ textAlign: 'center', fontSize: 14 }}>
-            Место для различнщй занимательнщй информации
+            <b>Место для различнoй занимательнoй информации</b>
           </Box>
         </Grid>
       </Grid>
